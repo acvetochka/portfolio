471c0ae50d3241b4112da696721acac7483bfc50
Alona Kuznietsova <acvetochka@gmail.com>
2024-11-03 18:04:17 +0100

    add About section

---
+++ src/sections/About/About.tsx
@@ -0,0 +1,12 @@
+import { Section } from "@/components/Section/Section"
+import { Text } from "@/components/Text/Text"
+
+export const About = ():JSX.Element => {
+    return (
+        <Section id="about" title="About me">
+            <Text>
+            I am a Frontend developer, focusing on HTML, CSS, JavaScript, and React, and I am constantly expanding my knowledge in this area. I have experience with the SASS preprocessor, BEM methodology, Tailwind CSS, and Emotion-styles. I can create adaptive layouts and use adaptive graphics in projects. Additionally, I have experience working as a Team Leader and as a Scrum Master in a team project. I am proficient in searching for and using information from various sources. I am able to quickly learn new activities and easily adapt to working with new computer programs. I possess an analytical mindset and have experience in remote work.
+            </Text>
+        </Section>
+    )
+}
