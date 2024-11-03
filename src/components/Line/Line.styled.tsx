c254885a86eeb2d015ce997c19ff7c2504a6b8e7
Alona Kuznietsova <acvetochka@gmail.com>
2024-11-03 18:02:20 +0100

    add Line component

---
+++ src/components/Line/Line.styled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const LineStyled = styled.div`
+height: 2px;
+width: 50%;
+background: linear-gradient(88deg, #a313e0 20%, #4f0ac9 66%, #0000ff);
+margin-bottom: 20px;
+`
