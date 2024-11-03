cb7f8b301d399fc46e738281ed911fac86006b7a
Alona Kuznietsova <acvetochka@gmail.com>
2024-11-03 18:05:08 +0100

    change HeroSection

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -2,12 +2,15 @@
 
 import { Section } from "@/components/Section/Section";
 import { Text } from "@/components/Text/Text";
+import { TextShadow } from "@/components/TextShadow/TextShadow";
 import { Title } from "@/components/Title/Title";
 
 export const HeroSection = ():JSX.Element => {
   return (
-    <Section  id="">
+    <Section  id="" className="heroSection">
+      <TextShadow size="54px" text="Alona Kuznietsova" id="title">
       <Title tag="h1">Alona Kuznietsova</Title>
+      </TextShadow>
       <Text size="extra-large">Frontend Developer</Text>
 
       {/* <button className="button">Download</button> */}
