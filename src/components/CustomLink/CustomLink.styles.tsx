6dfd4ac773b9f58b4236dbe7db931b01b0b8f189
Alona Kuznietsova <acvetochka@gmail.com>
2024-11-05 20:32:39 +0100

    add CustomLink component (#6)

---
+++ src/components/CustomLink/CustomLink.styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Link = styled.a`
+  display: inline-block;
+  width: fit-content;
+
+`
---
+++ src/components/CustomLink/CustomLink.tsx
@@ -0,0 +1,13 @@
+import { CustomLinkProps } from './CustomLink.props';
+import { Link } from './CustomLink.styles';
+
+export const CustomLink = ({
+  href,
+  children,
+}: CustomLinkProps): JSX.Element => {
+  return (
+    <Link href={href} target='_blank' rel='noopener noreferrer' className='link'>
+      {children}
+    </Link>
+  );
+};
