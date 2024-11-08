64f82306c5ae96b38c21cedee0e2927db5edea34
Alona Kuznietsova <acvetochka@gmail.com>
2024-11-08 23:16:39 +0100

    refactoring HeroSection
    move image to HeroImage Component

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -4,24 +4,30 @@ import { Section } from "@/components/Section/Section";
 import { Text } from "@/components/Text/Text";
 import { TextShadow } from "@/components/TextShadow/TextShadow";
 import { Title } from "@/components/Title/Title";
-import { ImageComtainer, ImageStyled } from "./HeroSection.styles";
+import { Button } from "@/components/Button/Button";
+import { HeroImage } from "@/components/HeroImage/HeroImage";
 // import { CustomLink } from "@/components/CustomLink/CustomLink";
 
 export const HeroSection = (): JSX.Element => {
   return (
     <Section id="" className="heroSection">
-      <TextShadow size="54px" text="Alona Kuznietsova" id="title">
-        <Title tag="h1">Alona Kuznietsova</Title>
-      </TextShadow>
-      <Text size="extra-large">Frontend Developer</Text>
-      <ImageComtainer>
-        <ImageStyled
-          src="/programmer.png"
-          alt="computer"
-          width={350}
-          height={300}
-        />
-      </ImageComtainer>
+      <div>
+        <TextShadow size="54px" text="Alona Kuznietsova" id="title">
+          <Title tag="h1">Alona Kuznietsova</Title>
+        </TextShadow>
+        <Text size="extra-large">Frontend Developer</Text>
+        <div>
+          <Button tag="scrollLink" link="skills">
+            Skills
+          </Button>
+          <Button tag="link" link="https://acvetochka.github.io/Resume/">
+            Resume
+          </Button>
+        </div>
+      </div>
+
+      <HeroImage/>
+
       {/* <CustomLink href="https://acvetochka.github.io/Resume/">CV</CustomLink> */}
     </Section>
   );
