803beb8fc28b5c5761629e9ebda842edecd11730
Alona Kuznietsova <acvetochka@gmail.com>
2024-11-08 23:15:42 +0100

    refactoring SkillsSection

--- src/sections/Skills/SkillsSection.tsx
+++ src/sections/Skills/SkillsSection.tsx
@@ -1,20 +1,10 @@
 import { Section } from "@/components/Section/Section"
-import { Title } from "@/components/Title/Title";
-import { Skills } from "@/components/Skills/Skills";
-import skillsData from "@/data/skills.json";
+import { SkillsList } from "@/components/SkillsList/SkillsList";
 
 export const SkillsSection = ():JSX.Element => {  
     return (
         <Section id="skills" title="Skills" className="skillsSection">
-            { skillsData.map(({id, type, skills})=> 
-            <div key={id}>
-                <Title tag="h3">{type}</Title>
-               <Skills skills={skills}/>
-               
-
-
-            </div>)
-            }
+           <SkillsList/>
         </Section>
     )
 }
