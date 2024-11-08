0bd930b38ffd91564905bb03f9dbd66f2aff6170
Alona Kuznietsova <acvetochka@gmail.com>
2024-11-09 00:58:37 +0100

    add HeroDescription component

---
+++ src/components/HeroDescription/HeroDescription.styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const DescriptionWrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+& > p {
+margin-top: 50px;
+}
+
+`
+
+export const ButtonWrapper = styled.div`
+margin-top: auto;
+display: flex;
+gap: 10px;
+`
--- src/components/HeroDescription/HeroDescription.tsx
+++ src/components/HeroDescription/HeroDescription.tsx
@@ -1,22 +1,23 @@
 import { Button, Text } from "@/components"
 import { TextShadow } from "@/components"
 import { Title } from "@/components"
+import { ButtonWrapper, DescriptionWrapper } from "./HeroDescription.styles"
 
 export const HeroDescription = ():JSX.Element => {
     return (
-        <div>
+        <DescriptionWrapper>
         <TextShadow size="54px" text="Alona Kuznietsova" id="title">
           <Title tag="h1">Alona Kuznietsova</Title>
         </TextShadow>
         <Text size="extra-large">Frontend Developer</Text>
-        <div>
+        <ButtonWrapper>
           <Button tag="scrollLink" link="skills">
             Skills
           </Button>
           <Button tag="link" link="https://acvetochka.github.io/Resume/">
             Resume
           </Button>
-        </div>
-      </div>
+        </ButtonWrapper>
+      </DescriptionWrapper>
     )
 }
