724acaf9c549960b970153a5afda44f7a9ecdbe9
Alona Kuznietsova <acvetochka@gmail.com>
2025-02-16 22:15:01 +0100

    change contacts

--- src/sections/Contacts/Contacts.tsx
+++ src/sections/Contacts/Contacts.tsx
@@ -3,6 +3,8 @@
 import {CustomLink, Section, Text } from "@/components";
 import contacts from "@/data/contacts.json"
 import { ItemStyled, ListStyled } from "./Contacts.styles";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa6";
 
 export const Contacts = (): JSX.Element => {
   return (
@@ -10,9 +12,11 @@ export const Contacts = (): JSX.Element => {
         <ListStyled>
             {contacts.map(({name,type,link}, id)=> (
                 <ItemStyled key={id}>
-                  {type !== "link" && <Text>{type}:</Text>}
+                  {type !== "link" ? <Text>{type}:</Text> : 
+                  <a href={link} target='_blank' rel='noopener noreferrer'>
+                    {name== "github" ? <AiOutlineGithub/> : <FaLinkedin/>}</a>}
                     <CustomLink href={link}>{name}</CustomLink>
-                </ItemStyled>
+                  </ItemStyled>
             ))}
             
         </ListStyled> 
