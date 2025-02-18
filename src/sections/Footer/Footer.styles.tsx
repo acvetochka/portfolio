f48de5bf88850c685e5b5cdfd3f85520b84197b5
Alona Kuznietsova <acvetochka@gmail.com>
2025-02-18 22:21:58 +0100

    add links to footer

---
+++ src/sections/Footer/Footer.styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ListStyled = styled.ul`
+  list-style: none;
+  padding-left: 0;
+  display: flex;
+//   flex-direction: column;
+  gap: 10px;
+  // align-items: center;
+`;
--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -5,6 +5,10 @@ import { Border, BorderContainer } from "../Header/Header.styles";
 import { Container } from "@/components/Section/Section.styled";
 import Image from "next/image";
 import Link from "next/link";
+import contacts from "@/data/contacts.json";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa6";
+import { ListStyled } from "./Footer.styles";
 
 export const Footer = (): JSX.Element => {
   return (
@@ -15,6 +19,16 @@ export const Footer = (): JSX.Element => {
             <Link href="/"><Image src="/logo.png" width={60} height={30} alt="logo"/></Link>
             {/* <Logo /> */}
             {/* <Navigation /> */}
+            <ListStyled>
+            {contacts.map(({link, name, type}, id)=> (
+                <li key={id}>
+                {type == "link" && (<><a href={link} target='_blank' rel='noopener noreferrer'>
+                {name === "github" ? <AiOutlineGithub width="25" height="25"/> : <FaLinkedin/>}</a></>)}
+                </li>
+            ))}
+            </ListStyled>
+            
+             
           </Border>
         </BorderContainer>
       </Container>
