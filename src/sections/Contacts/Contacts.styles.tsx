39b7b7f8b32dfa5b47bac9b63d0943bc70873280
Alona Kuznietsova <acvetochka@gmail.com>
2025-02-20 20:17:30 +0100

    change Contacts component

--- src/sections/Contacts/Contacts.styles.tsx
+++ src/sections/Contacts/Contacts.styles.tsx
@@ -1,20 +1,42 @@
 import styled from "styled-components";
 
+export const ContactWrapper = styled.div`
+@media (min-width: 768px) {
+align-items: center;
+}
+ @media (min-width: 1020px) {
+ flex-direction: row;
+ align-items: flex-start;
+ }
+
+
+display: flex;
+justify-content: space-between;
+flex-direction: column;
+gap: 30px;
+
+
+
+
+`
+
 export const ListStyled = styled.ul`
   list-style: none;
   padding-left: 0;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 20px;
   // align-items: center;
+  width: 300px;
 `;
 
 export const ItemStyled = styled.li`
   display: flex;
-  gap: 10px;
+  gap: 30px;
   align-items: center;
 
   & p{
 text-transform: capitalize;
+width: 70px;
   }
 `
--- src/sections/Contacts/Contacts.tsx
+++ src/sections/Contacts/Contacts.tsx
@@ -1,14 +1,16 @@
 "use client"
 
-import {CustomLink, Section, Text } from "@/components";
+import {ContactLinks, CustomLink, Section, Text } from "@/components";
 import contacts from "@/data/contacts.json"
-import { ItemStyled, ListStyled } from "./Contacts.styles";
+import { ContactWrapper, ItemStyled, ListStyled } from "./Contacts.styles";
+import { ContactForm } from "@/components/ContactForm/ContactForm";
 // import { AiOutlineGithub } from "react-icons/ai";
 // import { FaLinkedin } from "react-icons/fa6";
 
 export const Contacts = (): JSX.Element => {
   return (
-    <Section id="contacts" title="Contacts">
+    <Section id="contacts" title="Contact me">
+      <ContactWrapper>
         <ListStyled>
             {contacts.map(({name,type,link}, id)=> (
                 <ItemStyled key={id}>
@@ -16,8 +18,13 @@ export const Contacts = (): JSX.Element => {
                     <CustomLink href={link}>{name}</CustomLink>
                   </ItemStyled>
             ))}
-            
+            <ItemStyled>
+              <Text>Links:</Text>
+              <ContactLinks/>
+              </ItemStyled>
         </ListStyled> 
+        <ContactForm/>
+        </ContactWrapper>
     </Section>
   );
 };
