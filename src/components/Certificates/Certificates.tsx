813f79c8bbb7691218223bd2654005802d176ca2
Alona Kuznietsova <acvetochka@gmail.com>
2025-03-04 21:51:52 +0100

    fixed bug- delete import

--- src/components/Certificates/Certificates.tsx
+++ src/components/Certificates/Certificates.tsx
@@ -1,8 +1,8 @@
+import { AiOutlineExport } from "react-icons/ai";
+
 import { CustomLink, Title } from "@/components";
 import { CertItem, CertList, Wrapper } from "./Certificates.styles";
 import certData from "@/data/certification.json";
-import { VscGoToFile } from "react-icons/vsc";
-import { AiOutlineExport } from "react-icons/ai";
 
 export const Certificates = (): JSX.Element => {
   return (
