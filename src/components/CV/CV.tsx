2d0cb43b1ac9c3327b64c72b5f4680ff97758645
Alona Kuznietsova <acvetochka@gmail.com>
2025-03-09 19:32:59 +0100

    change SmileBar link

--- src/components/CV/CV.tsx
+++ src/components/CV/CV.tsx
@@ -9,7 +9,7 @@ export const CV = ():JSX.Element => {
     return (
         <ListStyled>
             <CVItem><CustomLink href="https://resume.kuznietsova.org">Show <TbWorldWww/></CustomLink></CVItem>
-            <CVItem><CustomLink href="/files/Frontend_Kuznietsova_Alona.pdf">Download (DE) <FaFilePdf/></CustomLink></CVItem>
+            <CVItem><CustomLink href="/files/Frontend_Kuznietsova_Alona.pdf">Download (de) <FaFilePdf/></CustomLink></CVItem>
         </ListStyled>
 
     )
