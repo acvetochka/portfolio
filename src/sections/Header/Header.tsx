50d79a51b55a32e90d7a527f5ef6a6dfa078604e
Alona Kuznietsova <acvetochka@gmail.com>
2025-03-17 15:18:26 +0100

    change media

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { MdMenu, MdClose } from "react-icons/md";
-// import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from "react-responsive";
 import { Navigation } from "@/components";
 import { MobileMenu } from "@/components/MobileMenu/MobileMenu";
 import { Container } from "@/components/Section/Section.styled";
@@ -12,14 +12,14 @@ import { Border, BorderContainer, MenuButton } from "./Header.styles";
 
 export const Header = (): JSX.Element => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  // const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   const [isClient, setIsClient] = useState(false);
 
-  const [isMobile, setIsMobile] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    setIsMobile(window.innerWidth <= 767);
-  }, []);
+  // useEffect(() => {
+  //   setIsMobile(window.innerWidth <= 767);
+  // }, []);
 
   useEffect(() => {
     setIsClient(true);
@@ -57,7 +57,7 @@ export const Header = (): JSX.Element => {
                 <Image src="/logo.png" width={60} height={30} alt="logo" />
               </Link>
               {/* <Logo /> */}
-              {isMobile ? (
+              {isClient && isMobile ? (
                 <MenuButton type="button" onClick={toggleMobileMenu}>
                   {isMobileMenuOpen ? (
                     <MdClose size={24} />
