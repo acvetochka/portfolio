0b028940deb2894286c1c8caaaf01106511f8320
Alona Kuznietsova <acvetochka@gmail.com>
2025-10-10 23:56:28 +0200

    add verify Email with MailboxLayer

---
+++ src/lib/fetchEmailValid.ts
@@ -0,0 +1,21 @@
+const API = process.env.NEXT_PUBLIC_APILAYER_URL;
+const API_KEY = process.env.NEXT_PUBLIC_MAILBOX_KEY;
+
+// 🔹 Простий Regex для базової валідації
+// export const isValidEmailFormat = (email: string) =>
+//   /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+// 🔹 Перевірка email через MailboxLayer
+export const verifyEmailWithMailboxLayer = async (email: string) => {
+  try {
+    const response = await fetch(
+      `${API}/check?access_key=${API_KEY}&email=${encodeURIComponent(email)}`
+    );
+    const data = await response.json();
+    console.log(data);
+
+    return data?.smtp_check === true && data?.format_valid === true;
+  } catch {
+    return false;
+  }
+};
