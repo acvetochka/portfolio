78b88e5954890bc2c06b0381cb47b0664d97d621
Alona Kuznietsova <acvetochka@gmail.com>
2026-06-22 22:31:33 +0200

    change skill-data-structure

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -7,8 +7,8 @@ import { IconContainer, SkillsListStyled } from "./Skills.styles";
 export const Skills = ({ skills }: SkillsProps): JSX.Element => {
   return (
     <SkillsListStyled>
-      {skills.map(({ id, name, icon }) => (
-        <li key={id}>
+      {skills.map(({ name, icon }) => (
+        <li key={name}>
           <IconContainer content={name}>
             <Image src={icon} width={40} height={40} alt={name} />
           </IconContainer>
